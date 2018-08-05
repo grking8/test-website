a059f81b3e6b129470699736fb7b7c1ed85d278f
family-guy <guy@zorncapital.com>
2018-08-05 21:43:25 +0100

    Correct var name

--- deploy-scripts/authenticate-gcloud.sh
+++ deploy-scripts/authenticate-gcloud.sh
@@ -6,7 +6,7 @@ set -e
 
 GCP_SERVICE_KEY_PATH="$HOME"/gcloud-service-key.json
 echo "$GCP_SERVICE_KEY" | base64 --decode --ignore-garbage > "$GCP_SERVICE_KEY_PATH"
-gcloud auth activate-service-account --key-file="$GCLOUD_SERVICE_KEY_PATH"
+gcloud auth activate-service-account --key-file="$GCP_SERVICE_KEY_PATH"
 gcloud config set project "$PROJECT_ID"
 gcloud config set compute/zone "$COMPUTE_ZONE"
 gcloud auth configure-docker
