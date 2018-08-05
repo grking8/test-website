3827168941b6dcb98b512a61ef25b065e5fb6050
family-guy <guy@zorncapital.com>
2018-08-05 20:54:21 +0100

    Add quiet option

--- deploy-scripts/get-nginx-config.sh
+++ deploy-scripts/get-nginx-config.sh
@@ -2,14 +2,10 @@
 
 set xe
 
-#cat >> ~/.ssh/config << EOF
-#  StrictHostKeyChecking no
-#EOF
-
 cd ../
 mkdir -p devops
 cd devops
-git clone git@gitlab.com:zorncapital-devops/website.git
+git clone --quiet git@gitlab.com:zorncapital-devops/website.git
 
 cp website/ssl/nginx/default.conf ../project/default.conf
 
