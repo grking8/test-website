a02b99b4470fb14aaca8abead34f1940ab7effd1
family-guy <guy@zorncapital.com>
2018-08-05 20:59:21 +0100

    Try fix

--- deploy-scripts/get-nginx-config.sh
+++ deploy-scripts/get-nginx-config.sh
@@ -5,7 +5,7 @@ set xe
 cd ../
 mkdir -p devops
 cd devops
-git clone --quiet git@gitlab.com:zorncapital-devops/website.git
+yes yes | git clone --quiet git@gitlab.com:zorncapital-devops/website.git
 
 cp website/ssl/nginx/default.conf ../project/default.conf
 
