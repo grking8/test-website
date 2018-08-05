5e532f24ce98461b81b3d1b8f911b3ac8a80394d
family-guy <guy@zorncapital.com>
2018-08-05 22:40:48 +0100

    Sub vars into nginx conf

--- deploy-scripts/get-nginx-config.sh
+++ deploy-scripts/get-nginx-config.sh
@@ -2,11 +2,13 @@
 
 set xe
 
+apt-get -y install gettext-base
+
 ssh-keyscan -H gitlab.com >> ~/.ssh/known_hosts
 cd ../
 mkdir -p devops
 cd devops
 git clone git@gitlab.com:zorncapital-devops/website.git
 
-cp website/ssl/nginx/default.conf ../project/default.conf
+envsubst < website/ssl/nginx/default.conf > ../project/default.conf
 
