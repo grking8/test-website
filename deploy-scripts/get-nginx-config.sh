7a1d24fa5a1dc2dc22e3772955c92867b6b4ea07
family-guy <guy@zorncapital.com>
2018-08-05 21:14:14 +0100

    Add gitlab as recognised host

--- deploy-scripts/get-nginx-config.sh
+++ deploy-scripts/get-nginx-config.sh
@@ -2,10 +2,11 @@
 
 set xe
 
+ssh-keyscan -H gitlab.com >> ~/.ssh/known_hosts
 cd ../
 mkdir -p devops
 cd devops
-yes yes | git clone --quiet git@gitlab.com:zorncapital-devops/website.git
+git clone git@gitlab.com:zorncapital-devops/website.git
 
 cp website/ssl/nginx/default.conf ../project/default.conf
 
