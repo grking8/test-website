6f483bd45793d05ed8cb8d4aee79af51ccae6785
family-guy <guy@zorncapital.com>
2018-08-06 07:08:54 +0100

    Update instance

--- deploy-scripts/update-instance.sh
+++ deploy-scripts/update-instance.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-set -e
+set -xe
 
 . deploy.cfg
 
-gcloud beta compute instances update-container $INSTANCE \
+gcloud beta compute instances update-container "$INSTANCE" \
   --container-image "$CONTAINER_REGISTRY"/"$PROJECT_ID"/"$CIRCLE_PROJECT_REPONAME":"$CIRCLE_SHA1"
 
