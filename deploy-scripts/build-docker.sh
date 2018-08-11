32dc7431d5ba0ca0ef9439a06d32e2f357f13958
family-guy <guy@zorncapital.com>
2018-08-11 13:30:31 +0100

    Use config var in dockerfile for webserver config

--- deploy-scripts/build-docker.sh
+++ deploy-scripts/build-docker.sh
@@ -5,4 +5,5 @@ set -xe
 . deploy.cfg
 docker build --tag $CONTAINER_REGISTRY/$PROJECT_ID/$CIRCLE_PROJECT_REPONAME:$CIRCLE_SHA1 \
              --build-arg SSL_CERTIFICATE_FILE=$CERTIFICATE_FILE \
-             --build-arg SSL_PRIVATE_KEY_FILE=$PRIVATE_KEY_FILE .
+             --build-arg SSL_PRIVATE_KEY_FILE=$PRIVATE_KEY_FILE \
+             --build-arg WEBSERVER_CONFIG_FILE=$WEBSERVER_CONFIG_FILE .
