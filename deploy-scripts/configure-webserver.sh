c0618096e8e1cb91750313373ed56a6e29333910
family-guy <guy@zorncapital.com>
2018-08-11 17:01:44 +0100

    Use correct base64

--- deploy-scripts/configure-webserver.sh
+++ deploy-scripts/configure-webserver.sh
@@ -5,7 +5,7 @@ set -e
 # create SSL files
 . deploy.cfg
 cd ../$DEPLOY_SCRIPTS_LIB_REPO/ssl
-./create-ssl-files.sh $SSL_CERTIFICATE $SSL_PRIVATE_KEY $SSL_CERTIFICATE_FILE $SSL_PRIVATE_KEY_FILE
+./create-ssl-files.sh true $SSL_CERTIFICATE $SSL_PRIVATE_KEY $SSL_CERTIFICATE_FILE $SSL_PRIVATE_KEY_FILE
 mkdir -p ../../project/config
 mv $SSL_PRIVATE_KEY_FILE ../../project/config
 mv $SSL_CERTIFICATE_FILE ../../project/config
