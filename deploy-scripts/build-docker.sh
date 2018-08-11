3da2308f4e90cca8bac94109c18360c7ddf50b28
family-guy <guy@zorncapital.com>
2018-08-11 17:55:38 +0100

    Make explicit path

--- deploy-scripts/build-docker.sh
+++ deploy-scripts/build-docker.sh
@@ -2,10 +2,10 @@
 
 set -xe
 
-echo $(pwd)
-. deploy.cfg
-mv config/* .
-rm -rf config
+DIR=/root/project/
+. ${DIR}deploy.cfg
+mv ${DIR}config/* .
+rm -rf ${DIR}config
 docker build --tag $CONTAINER_REGISTRY/$PROJECT_ID/$CIRCLE_PROJECT_REPONAME:$CIRCLE_SHA1 \
              --build-arg SSL_CERTIFICATE_FILE=$SSL_CERTIFICATE_FILE \
              --build-arg SSL_PRIVATE_KEY_FILE=$SSL_PRIVATE_KEY_FILE .
