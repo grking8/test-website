a1c590394a7a58f5b320594b435258867e96866b
family-guy <guy@zorncapital.com>
2018-08-11 14:54:44 +0100

    Correct var name

--- deploy-scripts/create-ssl-files.sh
+++ deploy-scripts/create-ssl-files.sh
@@ -5,5 +5,5 @@ set -e
 . deploy.cfg
 cd ../$DEPLOY_SCRIPTS_LIB_REPO/ssl
 ./create-ssl-files.sh $SSL_CERTIFICATE $SSL_PRIVATE_KEY $SSL_CERTIFICATE_FILE $SSL_PRIVATE_KEY_FILE
-mv $SSL_PRIVATE_KEY ../../project
+mv $SSL_PRIVATE_KEY_FILE ../../project
 mv $SSL_CERTIFICATE_FILE ../../project
