39a9d555fd0ba2b3b6248eb3ccd8977f62717740
family-guy <guy@zorncapital.com>
2018-08-12 11:59:50 +0100

    Supply missing args

--- deploy-scripts/update-instance.sh
+++ deploy-scripts/update-instance.sh
@@ -6,7 +6,7 @@ DIR=/root/project/
 . ${DIR}deploy.cfg
 cd ..
 # authenticate GCP
-${DEPLOY_SCRIPTS_LIB_REPO}/gcp/authenticate.sh
+${DEPLOY_SCRIPTS_LIB_REPO}/gcp/authenticate.sh "$GCP_SERVICE_KEY" $GCP_PROJECT_ID $GCP_COMPUTE_ZONE
 # update instance
 gcloud beta compute instances update-container $GCP_INSTANCE \
   --container-image ${GCP_CONTAINER_REGISTRY}/${GCP_PROJECT_ID}/${CIRCLE_PROJECT_REPONAME}:${CIRCLE_SHA1}
