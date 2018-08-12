3fa230193fc1c7da48320eef05d724894018369b
family-guy <guy@zorncapital.com>
2018-08-12 10:43:20 +0100

    Refactor deploy prod job

--- deploy-scripts/containerise.sh
+++ deploy-scripts/containerise.sh
@@ -8,9 +8,7 @@ DIR=/root/project/
 mv ${DIR}config/* .
 rm -rf ${DIR}config
 cd  ..
-
 # autthenticate GCP
 ${DEPLOY_SCRIPTS_LIB_REPO}/gcp/authenticate.sh
-
 # build Docker image and push to container registry
 ${DEPLOY_SCRIPTS_LIB_REPO}/build-push-docker.sh
--- deploy-scripts/update-instance.sh
+++ deploy-scripts/update-instance.sh
@@ -4,5 +4,8 @@ set -xe
 
 DIR=/root/project/
 . ${DIR}deploy.cfg
+# autthenticate GCP
+${DEPLOY_SCRIPTS_LIB_REPO}/gcp/authenticate.sh
+# update instance
 gcloud beta compute instances update-container $INSTANCE \
   --container-image ${CONTAINER_REGISTRY}/${PROJECT_ID}/${CIRCLE_PROJECT_REPONAME}:${CIRCLE_SHA1}
