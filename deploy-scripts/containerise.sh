c68d5409ae98f337311ca961093aaca7b6aeb73e
family-guy <guy@zorncapital.com>
2018-08-12 12:18:05 +0100

    Export vars rather than pass args to scripts

--- deploy-scripts/containerise.sh
+++ deploy-scripts/containerise.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env sh
 
-set -e
+set -xe
 
-# prepare files
 DIR=/root/project/
 export DIR
 . ${DIR}deploy.cfg
 mv ${DIR}config/* .
 rm -rf ${DIR}config
 cd  ..
-# authenticate GCP
-${DEPLOY_SCRIPTS_LIB_REPO}/gcp/authenticate.sh "$GCP_SERVICE_KEY" $GCP_PROJECT_ID $GCP_COMPUTE_ZONE
-# build Docker image and push to container registry
-${DEPLOY_SCRIPTS_LIB_REPO}/build-push-docker.sh $GCP_CONTAINER_REGISTRY $GCP_PROJECT_ID $SSL_CERTIFICATE_FILE $SSL_PRIVATE_KEY_FILE
+${DEPLOY_SCRIPTS_LIB_REPO}/gcp/authenticate.sh
+${DEPLOY_SCRIPTS_LIB_REPO}/build-push-docker.sh
--- deploy-scripts/update-instance.sh
+++ deploy-scripts/update-instance.sh
@@ -5,8 +5,6 @@ set -xe
 DIR=/root/project/
 . ${DIR}deploy.cfg
 cd ..
-# authenticate GCP
-${DEPLOY_SCRIPTS_LIB_REPO}/gcp/authenticate.sh "$GCP_SERVICE_KEY" $GCP_PROJECT_ID $GCP_COMPUTE_ZONE
-# update instance
+${DEPLOY_SCRIPTS_LIB_REPO}/gcp/authenticate.sh
 gcloud beta compute instances update-container $GCP_INSTANCE \
   --container-image ${GCP_CONTAINER_REGISTRY}/${GCP_PROJECT_ID}/${CIRCLE_PROJECT_REPONAME}:${CIRCLE_SHA1}
