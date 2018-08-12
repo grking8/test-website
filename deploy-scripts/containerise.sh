74743e1ed1ab8d9f29e606045400da72d8905911
family-guy <guy@zorncapital.com>
2018-08-12 10:28:08 +0100

    Refactor into single containerise script

---
+++ deploy-scripts/containerise.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+set -xe
+
+# prepare files
+DIR=/root/project/
+. ${DIR}deploy.cfg
+mv ${DIR}config/* .
+rm -rf ${DIR}config
+cd  ..
+
+# autthenticate GCP
+${DEPLOY_SCRIPTS_LIB_REPO}/gcp/authenticate.sh
+
+# build Docker image and push to container registry
+${DEPLOY_SCRIPTS_LIB_REPO}/build-push-docker.sh
--- deploy-scripts/push-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-set -xe
-
-DIR=/root/project/
-. ${DIR}deploy.cfg
-docker push ${CONTAINER_REGISTRY}/${PROJECT_ID}/${CIRCLE_PROJECT_REPONAME}:${CIRCLE_SHA1}
