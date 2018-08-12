57b725c6b74c5a8eb10f1f3025e5208a2196fb5c
family-guy <guy@zorncapital.com>
2018-08-12 11:24:04 +0100

    Update for new lib scripts

--- deploy-scripts/containerise.sh
+++ deploy-scripts/containerise.sh
@@ -8,7 +8,7 @@ DIR=/root/project/
 mv ${DIR}config/* .
 rm -rf ${DIR}config
 cd  ..
-# autthenticate GCP
-${DEPLOY_SCRIPTS_LIB_REPO}/gcp/authenticate.sh
+# authenticate GCP
+${DEPLOY_SCRIPTS_LIB_REPO}/gcp/authenticate.sh $GCP_SERVICE_KEY $GCP_PROJECT_ID $GCP_COMPUTE_ZONE
 # build Docker image and push to container registry
-${DEPLOY_SCRIPTS_LIB_REPO}/build-push-docker.sh
+${DEPLOY_SCRIPTS_LIB_REPO}/build-push-docker.sh $GCP_CONTAINER_REGISTRY $GCP_PROJECT_ID $SSL_CERTIFICATE_FILE $SSL_PRIVATE_KEY_FILE
--- deploy-scripts/update-instance.sh
+++ deploy-scripts/update-instance.sh
@@ -4,8 +4,8 @@ set -xe
 
 DIR=/root/project/
 . ${DIR}deploy.cfg
-# autthenticate GCP
+# authenticate GCP
 ${DEPLOY_SCRIPTS_LIB_REPO}/gcp/authenticate.sh
 # update instance
-gcloud beta compute instances update-container $INSTANCE \
-  --container-image ${CONTAINER_REGISTRY}/${PROJECT_ID}/${CIRCLE_PROJECT_REPONAME}:${CIRCLE_SHA1}
+gcloud beta compute instances update-container $GCP_INSTANCE \
+  --container-image ${GCP_CONTAINER_REGISTRY}/${GCP_PROJECT_ID}/${CIRCLE_PROJECT_REPONAME}:${CIRCLE_SHA1}
