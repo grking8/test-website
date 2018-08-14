b890366352b6739ce928b03fee8196cbef52d662
family-guy <guy@zorncapital.com>
2018-08-14 20:22:34 +0100

    Try again

--- deploy-scripts/external-libs.sh
+++ deploy-scripts/external-libs.sh
@@ -9,8 +9,8 @@ if [ $1 = true ]; then
     apk add openssh-client
     apk add git
 fi
-mkdir -p ~/.ssh
-ssh-keyscan -H $EXTERNAL_GITHOST >> ~/.ssh/known_hosts
-cd ..
-git clone $WEBSERVER_LIB_URL
-git clone $DEPLOY_SCRIPTS_LIB_URL
+#mkdir -p ~/.ssh
+#ssh-keyscan -H $EXTERNAL_GITHOST >> ~/.ssh/known_hosts
+#cd ..
+#git clone $WEBSERVER_LIB_URL
+#git clone $DEPLOY_SCRIPTS_LIB_URL
