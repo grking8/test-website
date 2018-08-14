f645f52dabd25f8ae6d29e336980426fc55d6d10
family-guy <guy@zorncapital.com>
2018-08-14 20:31:38 +0100

    Test

--- deploy-scripts/external-libs.sh
+++ deploy-scripts/external-libs.sh
@@ -4,11 +4,11 @@ set -e
 
 DIR=/root/project/
 . ${DIR}deploy.cfg
-if [ $1 = true ]; then
-#    apk update
-#    apk add openssh-client
-    apk add git
-fi
+#if [ $1 = true ]; then
+##    apk update
+##    apk add openssh-client
+#    apk add git
+#fi
 #mkdir -p ~/.ssh
 #ssh-keyscan -H $EXTERNAL_GITHOST >> ~/.ssh/known_hosts
 #cd ..
