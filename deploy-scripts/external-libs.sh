29eb3577b193555f17708dbc40e2ce7817f44ff9
family-guy <guy@zorncapital.com>
2018-09-09 20:23:01 +0100

    Remove all code associated with "build" step

--- deploy-scripts/external-libs.sh
+++ deploy-scripts/external-libs.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-#DIR=/root/project/
-#. ${DIR}deploy.cfg
+DIR=/root/project/
+. ${DIR}deploy.cfg
 if [ $1 = true ]; then
-#    apk update
-#    apk add openssh-client
+    apk update
+    apk add openssh-client
     apk add git
 fi
-#mkdir -p ~/.ssh
-#ssh-keyscan -H $EXTERNAL_GITHOST >> ~/.ssh/known_hosts
-#cd ..
-#git clone $WEBSERVER_LIB_URL
-#git clone $DEPLOY_SCRIPTS_LIB_URL
+mkdir -p ~/.ssh
+ssh-keyscan -H $EXTERNAL_GITHOST >> ~/.ssh/known_hosts
+cd ..
+git clone $WEBSERVER_LIB_URL
+git clone $DEPLOY_SCRIPTS_LIB_URL
