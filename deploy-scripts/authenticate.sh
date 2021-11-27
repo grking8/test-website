c02a2a7de0a699969aa69eff87dd105bf43badee
family-guy <guy@zorncapital.com>
2021-11-27 17:12:34 +0000

    ci: debug

--- deploy-scripts/authenticate.sh
+++ deploy-scripts/authenticate.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+echo hello
+echo "<< pipeline.parameters.gcp-identity-service-account-key-path >>"
 echo "${GCP_IDENTITY_SERVICE_ACCOUNT_KEY}" | base64 --decode --ignore-garbage > "<< pipeline.parameters.gcp-identity-service-account-key-path >>"
 gcloud auth activate-service-account "$GCP_IDENTITY_SERVICE_ACCOUNT_EMAIL" --key-file="${GCP_IDENTITY_SERVICE_ACCOUNT_KEY_PATH}"
 gcloud config set project "$GCP_PROJECT_ID"
