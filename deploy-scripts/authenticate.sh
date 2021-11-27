8878320a3cc54a629b7bbb2489b8ffb98a1ee358
family-guy <guy@zorncapital.com>
2021-11-27 17:08:18 +0000

    test: dont use export

--- deploy-scripts/authenticate.sh
+++ deploy-scripts/authenticate.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo "${GCP_IDENTITY_SERVICE_ACCOUNT_KEY}" | base64 --decode --ignore-garbage > "$GCP_IDENTITY_SERVICE_ACCOUNT_KEY_PATH"
+echo "${GCP_IDENTITY_SERVICE_ACCOUNT_KEY}" | base64 --decode --ignore-garbage > "<< pipeline.parameters.gcp-identity-service-account-key-path >>"
 gcloud auth activate-service-account "$GCP_IDENTITY_SERVICE_ACCOUNT_EMAIL" --key-file="${GCP_IDENTITY_SERVICE_ACCOUNT_KEY_PATH}"
 gcloud config set project "$GCP_PROJECT_ID"
 gcloud config set auth/impersonate_service_account "$GCP_RESOURCE_SERVICE_ACCOUNT_EMAIL"
