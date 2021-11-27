a09899a0ee9c2816b3a3e17076456c234c14898b
family-guy <guy@zorncapital.com>
2021-11-27 17:15:57 +0000

    chore: remove debugging;

--- deploy-scripts/authenticate.sh
+++ deploy-scripts/authenticate.sh
@@ -3,8 +3,7 @@
 set -e
 
 echo hello
-echo "<< pipeline.parameters.gcp-identity-service-account-key-path >>"
-echo "${GCP_IDENTITY_SERVICE_ACCOUNT_KEY}" | base64 --decode --ignore-garbage > "<< pipeline.parameters.gcp-identity-service-account-key-path >>"
+echo "${GCP_IDENTITY_SERVICE_ACCOUNT_KEY}" | base64 --decode --ignore-garbage > "${GCP_IDENTITY_SERVICE_ACCOUNT_KEY_PATH}"
 gcloud auth activate-service-account "$GCP_IDENTITY_SERVICE_ACCOUNT_EMAIL" --key-file="${GCP_IDENTITY_SERVICE_ACCOUNT_KEY_PATH}"
 gcloud config set project "$GCP_PROJECT_ID"
 gcloud config set auth/impersonate_service_account "$GCP_RESOURCE_SERVICE_ACCOUNT_EMAIL"
